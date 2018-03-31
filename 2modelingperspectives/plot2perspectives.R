caf91e735fd6baf78328b5701f6e6fcf831708c2
tedunderwood <tunder@illinois.edu>
2018-03-31 11:17:01 -0500

    fine-tuning figure 2

--- 2modelingperspectives/plot2perspectives.R
+++ 2modelingperspectives/plot2perspectives.R
@@ -12,16 +12,16 @@ p <- ggplot(persp, aes(m, f)) +
   geom_point(alpha = 0.5) +
   scale_y_continuous('m   <--   in books by women  -->     f', limits = c(-15, 15)) +
   scale_x_continuous('m   <--   in books by men   -->    f', limits = c(-15, 15)) +
-  geom_text_repel(aes(m, f, label = label), force = 2, 
-                  point.padding = 0.5, max.iter = 1000, size = 3.5,
+  geom_text_repel(aes(m, f, label = label), force = 7, 
+                  point.padding = 0.7, max.iter = 1200, size = 4.2,
                   family = "Avenir Next Medium") +
   ggtitle('Words overrepresented in description\nof masculine or feminine characters, 1780-2009') +
   theme(text = element_text(size = 16, family = "Avenir Next Medium"), 
         panel.border = element_blank(),
         axis.line = element_line(color = 'black'),
         plot.title = element_text(margin = margin(b = 14), size = 16, lineheight = 1.1)) +
-  annotate('text', x = - 5, y = -14, label = "Overrepresentation measured as\nthe log of Dunning's log-likelihood", 
-           hjust = 0, family = 'Avenir Next Medium', size = 4)
+  annotate('text', x = -2.5 , y = -13.5, label = "Overrepresentation measured as\nthe signed log of Dunning's log-likelihood", 
+           hjust = 0, family = 'Avenir Next Medium', size = 3.9)
 
 tiff("fig2plot2perspectives.tiff", height = 8, width = 8, units = 'in', res=400)
 plot(p)
