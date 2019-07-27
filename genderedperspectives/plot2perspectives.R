f0a1468b9cb749580685a1015d386d2da52bd83c
tedunderwood <tunder@illinois.edu>
2019-07-27 17:08:24 -0500

    new plot parameters

--- genderedperspectives/plot2perspectives.R
+++ genderedperspectives/plot2perspectives.R
@@ -12,18 +12,18 @@ p <- ggplot(persp, aes(m, f)) +
   geom_point(alpha = 0.5) +
   scale_y_continuous('m   <--   in books by women  -->     f', limits = c(-15.5, 15.5)) +
   scale_x_continuous('m   <--   in books by men   -->    f', limits = c(-15.5, 15.5)) +
-  geom_text_repel(aes(m, f, label = label), force = 6, box.padding = 0.6, 
-                  point.padding = 0.5, max.iter = 1200, size = 4.2,
+  geom_text_repel(aes(m, f, label = label), force = 6, box.padding = 0.3, 
+                  point.padding = 0.8, max.iter = 1200, size = 4.2,
                   family = "Avenir Next Medium") +
-  ggtitle('Words overrepresented in description\nof masculine or feminine characters, 1780-2009') +
   theme(text = element_text(size = 16, family = "Avenir Next Medium"), 
         panel.border = element_blank(),
         axis.line = element_line(color = 'black'),
-        plot.title = element_text(margin = margin(b = 14), size = 16, lineheight = 1.1)) +
-  annotate('text', x = -2.5 , y = -14.5, label = "Overrepresentation measured as\nthe signed log of Dunning's log-likelihood", 
-           hjust = 0, family = 'Avenir Next Medium', size = 3.9)
+        plot.title = element_text(margin = margin(b = 14), size = 16, lineheight = 1.1))
 
-tiff("fig2plot2perspectives.tiff", height = 8, width = 8, units = 'in', res=400)
+# annotate('text', x = -2.5 , y = -14.5, label = "Overrepresentation measured as\nthe signed log of Dunning's log-likelihood", 
+# hjust = 0, family = 'Avenir Next Medium', size = 3.9)
+
+tiff("fig1trial.tiff", height = 8, width = 8, units = 'in', res=400)
 plot(p)
 dev.off()
 plot(p)
--- genderedperspectives/readme.md
+++ genderedperspectives/readme.md
@@ -1,13 +1,13 @@
 Section 2: Modeling Perspectives
 =================================
 
-Code used to generate figure 2, which explores the divergence of two different perspectives on gender.
+Code used to generate figure 1, which explores the divergence of two different perspectives on gender.
 
 The raw data on characters is drawn from ["The Transformation of Gender in English-Language Fiction",](http://culturalanalytics.org/2018/02/the-transformation-of-gender-in-english-language-fiction/) and can be downloaded from [the *Cultural Analytics* Dataverse for the article.](https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/ZM2MAN) It's quite voluminous (several gigabytes) and won't fit easily in github.
 
 But this directory does contain the summary data I used to produce figure 2 (**chartable.tsv.zip**), as well as the script (**create_character_table.py**) that derived it from the raw data.
 
-The full sequence of steps is as follows:
+If you want to reproduce the process from as close to raw data as I can provide consistent with IP law, start with steps 1 and 2. Otherwise, unzip **chartable.tsv** and start with step 3:
 
 1. From the CA Dataverse, get **character_table_18c19c.tsv** and **character_table_post1900.tsv**.
 
